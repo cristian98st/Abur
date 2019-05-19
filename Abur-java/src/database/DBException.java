c7e592f82350d3f638aa426a034a337f9560d229
AlexandruPutanu <alexandruputanu@yahoo.com>
2019-05-19 15:14:57 +0300

    Deleted abur

---
+++ Abur-java/src/database/DBException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package database;
+
+/**
+ * @author Alex
+ *
+ */
+public class DBException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DBException(String error) {
+		super(error);
+	}
+}
