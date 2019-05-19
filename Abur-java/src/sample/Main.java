d8767a8de36d5edc49a098b4bea205ab53654b6e
Cristian Tugui <cris_st7@yahoo.com>
2019-05-19 13:40:13 +0300

    Items added.

--- Abur-java/src/sample/Main.java
+++ Abur-java/src/sample/Main.java
@@ -35,6 +35,8 @@ public class Main extends Application implements Initializable {
     private JFXButton btnMyAccount, btnMyGames, btnMyItems, btnSettings, btnLogout;
     @FXML
     private TableView<Game> gamesTable;
+    @FXML
+    private TableView<Item> itemsTable;
 
     static Stage stage = new Stage();
 
@@ -95,11 +97,13 @@ public class Main extends Application implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
-        TableColumn<Game, String> id = new TableColumn<>("ID");
-        id.setPrefWidth(262);
-        id.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
 
-        id.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>() {
+        // Games
+        TableColumn<Game, String> gameID = new TableColumn<>("ID");
+        gameID.setPrefWidth(262);
+        gameID.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+        gameID.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>() {
             @Override
             public ObservableValue<String> call(TableColumn.CellDataFeatures<Game, String> param) {
                 return param.getValue().id;
@@ -118,12 +122,12 @@ public class Main extends Application implements Initializable {
             }
         });
 
-        TableColumn<Game, String> price = new TableColumn<>("Price");
-        price.setPrefWidth(262);
-        price.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+        TableColumn<Game, String> gamePrice = new TableColumn<>("Price");
+        gamePrice.setPrefWidth(262);
+        gamePrice.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
 
 
-        price.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>() {
+        gamePrice.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>() {
             @Override
             public ObservableValue<String> call(TableColumn.CellDataFeatures<Game, String> param) {
                 return param.getValue().price;
@@ -148,11 +152,101 @@ public class Main extends Application implements Initializable {
         games.add(new Game("3", "AC3", "104", "2/12/2006"));
         games.add(new Game("4", "AC4", "1005", "2/12/2007"));
 
-        final TreeItem<Game> root = new RecursiveTreeItem<Game>(games, RecursiveTreeObject::getChildren);
-        gamesTable.getColumns().setAll(id, title, price, date);
-//        gamesTable.setRoot
-//        gamesTable.setShowRoot(false);
+        gamesTable.getColumns().setAll(gameID, title, gamePrice, date);
         gamesTable.setItems(games);
+
+
+        //items
+        TableColumn<Item, String> itemID = new TableColumn<>("ID");
+        itemID.setPrefWidth(150);
+        itemID.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+        itemID.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().id;
+            }
+        });
+
+        TableColumn<Item, String> itemName = new TableColumn<>("Item Name");
+        itemName.setPrefWidth(150);
+        itemName.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        itemName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().itemName;
+            }
+        });
+
+        TableColumn<Item, String> classa = new TableColumn<>("Class");
+        classa.setPrefWidth(150);
+        classa.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        classa.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().itemPrice;
+            }
+        });
+
+        TableColumn<Item, String> typeOf = new TableColumn<>("Type Of");
+        typeOf.setPrefWidth(150);
+        typeOf.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        typeOf.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().typeOf;
+            }
+        });
+
+        TableColumn<Item, String> wear = new TableColumn<>("Wear");
+        wear.setPrefWidth(150);
+        wear.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        wear.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().wear;
+            }
+        });
+
+        TableColumn<Item, String> rarity = new TableColumn<>("Rarity");
+        rarity.setPrefWidth(150);
+        rarity.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        rarity.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().rarity;
+            }
+        });
+
+        TableColumn<Item, String> itemPrice = new TableColumn<>("Price");
+        itemPrice.setPrefWidth(150);
+        itemPrice.setStyle("-fx-background-color: #323232; -fx-text-fill: #fff");
+
+
+        itemPrice.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return param.getValue().itemPrice;
+            }
+        });
+
+        ObservableList<Item> items = FXCollections.observableArrayList();
+        items.add(new Item("1", "Sword1", "Warrior", "melee", "destroyed", "rare", "100"));
+        items.add(new Item("2", "Bow", "Hunter", "range", "good", "common", "700"));
+
+        gamesTable.getColumns().setAll(gameID, title, gamePrice, date);
+        itemsTable.getColumns().setAll(itemID, itemName, classa, typeOf, wear, rarity, itemPrice);
+        itemsTable.setItems(items);
     }
 
     class Game extends RecursiveTreeObject<Game>{
@@ -169,31 +263,23 @@ public class Main extends Application implements Initializable {
         }
     }
 
-//    public static void autoResizeColumns( TreeTableView<?> table )
-//    {
-//        //Set the right policy
-//        table.setColumnResizePolicy( TreeTableView.UNCONSTRAINED_RESIZE_POLICY);
-//        table.getColumns().stream().forEach( (column) ->
-//        {
-//            //Minimal width = columnheader
-//            Text t = new Text( column.getText() );
-//            double max = t.getLayoutBounds().getWidth();
-//            for ( int i = 0; i < table.getTreeItem().size(); i++ )
-//            {
-//                //cell must not be empty
-//                if ( column.getCellData( i ) != null )
-//                {
-//                    t = new Text( column.getCellData( i ).toString() );
-//                    double calcwidth = t.getLayoutBounds().getWidth();
-//                    //remember new max-width
-//                    if ( calcwidth > max )
-//                    {
-//                        max = calcwidth;
-//                    }
-//                }
-//            }
-//            //set the new max-widht with some extra space
-//            column.setPrefWidth( max + 10.0d );
-//        } );
-//    }
+    class Item extends RecursiveTreeObject<Item>{
+        StringProperty id;
+        StringProperty itemName;
+        StringProperty classa;
+        StringProperty typeOf;
+        StringProperty wear;
+        StringProperty rarity;
+        StringProperty itemPrice;
+
+        public Item(String id, String itemName, String classa, String typeOf, String wear, String rarity, String itemPrice){
+            this.id = new SimpleStringProperty(id);
+            this.itemName = new SimpleStringProperty(itemName);
+            this.classa = new SimpleStringProperty(classa);
+            this.typeOf = new SimpleStringProperty(typeOf);
+            this.wear = new SimpleStringProperty(wear);
+            this.rarity = new SimpleStringProperty(rarity);
+            this.itemPrice = new SimpleStringProperty(itemPrice);
+        }
+    }
 }
